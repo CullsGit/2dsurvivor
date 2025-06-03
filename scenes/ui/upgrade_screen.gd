6e55f3cd8eb3c22bc7c09bf7d81ddcbb7087a7ab
Hugo Cullinan <hugo.cullinan@gmail.com>
2025-06-03 16:21:30 +1000

    add functionality to select upgrade and add it to current upgrades

--- scenes/ui/upgrade_screen.gd
+++ scenes/ui/upgrade_screen.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+signal upgrade_selected(upgrade: AbilityUpgrade)
+
 @export var upgrade_card_scene: PackedScene
 
 @onready var card_container: HBoxContainer = %CardContainer
@@ -15,4 +17,7 @@ func set_ability_upgrades(upgrades: Array[AbilityUpgrade]):
 		card_instance.selected.connect(on_upgrade_selected.bind(upgrade))
 
 func on_upgrade_selected(upgrade: AbilityUpgrade):
-	pass
+	upgrade_selected.emit(upgrade)
+	get_tree().paused = false
+	queue_free()
+	
